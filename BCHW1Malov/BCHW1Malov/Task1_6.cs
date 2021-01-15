85fd25599ea3f53f2e349afe4c12d97f8d9ab30c
Alexander <KeysiRayback@gmail.com>
2021-01-15 15:33:51 +0300

    Added home work for lesson 1

---
+++ BCHW1Malov/BCHW1Malov/Task1_6.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BCHW1Malov
+{
+	public class Task1_6
+	////6. *Создать класс с методами, которые могут пригодиться в вашей учебе (Print, Pause).
+
+	{
+
+		public Task1_6()
+		{
+		}
+		public void PrintPlease(string str, int x, int y)
+		{
+			Console.SetCursorPosition(x, y);
+			Console.WriteLine(str);
+		}
+		public void Pouse()
+        {
+			Console.ReadLine();
+        }
+		public void Clear()
+        {
+			Console.Clear();
+        }
+	}
+}
